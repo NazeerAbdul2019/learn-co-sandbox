9690cdc81489b49cb53858ae2ac48f6684be67e5
voracious-module-9516 <as.nazeer2019@gmail.com>
2020-02-03 17:14:18 +0000

    Automatically backed up by Learn

---
+++ test.rb
@@ -0,0 +1,37 @@
+#check age of customer
+#determine if customer is underage
+#legal drinking age >= 21
+#if age < 21 you cannot sell liquor to customer
+
+def legal_drinking_age(age)
+  if age < 21
+    puts "Go Away Kid!"
+  end
+  if age >= 21
+    puts "Welcome!"
+  end
+end
+
+
+def combined_age_of_spouses(age)
+  x = 20
+  y = 30
+  if (x + y) > 50
+    puts "Too OLD, GO BACK HOME!"
+  else
+    puts "COME CHILL WITH US"
+  end
+end
+
+age (44)
+
+10. times do
+  puts "Hi, welcome to my very repetitive program!"
+end
+
+
+
+
+
+
+
